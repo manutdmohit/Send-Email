584eb28610d8631a31667e5875b1683bd6c68fce
manutdmohit <saudmohit@gmail.com>
2022-02-26 14:58:07 +0545

    Send emails with ethereal

--- controllers/sendEmail.js
+++ controllers/sendEmail.js
@@ -1,3 +1,23 @@
+const nodemailer = require('nodemailer');
+
 exports.sendEmail = async (req, res) => {
-  res.send('send email');
+  let testAccount = await nodemailer.createTestAccount();
+
+  const transporter = nodemailer.createTransport({
+    host: process.env.HOST_NAME,
+    port: process.env.PORT_NUMBER,
+    auth: {
+      user: process.env.USER,
+      pass: process.env.PASS,
+    },
+  });
+
+  let info = await transporter.sendMail({
+    from: '"Revolution 2022" <revolution2022@gmail.com',
+    to: 'bar@example.com',
+    subject: 'Hello',
+    html: '<h2>Sending Emails with Node.js </h2>',
+  });
+
+  res.json(info);
 };
